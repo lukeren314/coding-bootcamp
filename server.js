a54ee9206653f1d846b95d2c2d447c11cc202cea
Luke Ren <lukeren314@gmail.com>
2022-03-15 18:03:03 -0700

    config for heroku

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
 const path = require("path");
 const routes = require("./routes"); // import routes from routes/index.js
 
